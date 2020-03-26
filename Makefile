ec5970135ebe34781a1dc82d4df8923ab1ecc9a9
Sebastian <sebastiancolandre@gmail.com>
2020-03-25 23:22:33 -0400

    agregamos mensaje y contador

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 bin/servicio: bin/servicio.o 
-	clang bin/servicio.o -o bin/servicio
+	clang bin/servicio.o -lmicrohttpd -lm -o bin/servicio
 
 bin/servicio.o: servicio.c
 	mkdir -p bin
 	clang servicio.c -c -o bin/servicio.o
+
--- servicio.c
+++ servicio.c
@@ -1,3 +1,48 @@
-int main(int argc, char **argv){
+#include <microhttpd.h>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define MENSAJE "<html><body>" \
+                       "<h1>Fibonacci</h1>" \
+                       "El %d n&uacute;mero es %d." \
+                       "</body></html>"
+
+int responder(void *cls, struct MHD_Connection *connection,
+              const char *url,
+              const char *method, const char *version,
+              const char *upload_data,
+              size_t *upload_data_size, void **con_cls)
+{
+    static int numero = 1;
+    int tamanio = sizeof (MENSAJE)+log(numero)/log(10)+log(numero)/log(10);
+    char *page = malloc(tamanio);
+    int ret;
+    struct MHD_Response *response;
+    sprintf(page, MENSAJE, numero, numero);
+    numero ++;
+    response = MHD_create_response_from_buffer(strlen(page),
+                                               (void *)page, MHD_RESPMEM_PERSISTENT);
+    MHD_add_response_header(response, MHD_HTTP_HEADER_CONTENT_TYPE, "text/html");
+    ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
+    MHD_destroy_response(response);
+    free(page);
+    return ret;
+}
+
+int main(int argc, char **argv)
+{
+    struct MHD_Daemon *daemon;
+    daemon = MHD_start_daemon(MHD_USE_INTERNAL_POLLING_THREAD,
+                              8080,
+                              NULL,
+                              NULL,
+                              &responder,
+                              NULL,
+                              MHD_OPTION_END);
+    getchar();
+    MHD_stop_daemon(daemon);
+
     return 0;
 }
